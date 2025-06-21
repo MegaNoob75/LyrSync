f41508a38621daad112d771c079d315a6709bbcd
ross7 <ross7@ROSS-PC>
2025-06-21 07:50:49 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,240 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        btnSelectFolder = New Button()
+        btnStart = New Button()
+        lstLog = New ListBox()
+        ProgressBar = New ProgressBar()
+        lblProgress = New Label()
+        txtFolder = New TextBox()
+        lblFailed = New Label()
+        lvFileResults = New ListView()
+        ColumnHeader1 = New ColumnHeader()
+        ColumnHeader2 = New ColumnHeader()
+        numMaxConcurrent = New NumericUpDown()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        Label2 = New Label()
+        Label3 = New Label()
+        chkFallbackUnsynced = New CheckBox()
+        CType(numMaxConcurrent, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' btnSelectFolder
+        ' 
+        btnSelectFolder.ForeColor = SystemColors.ActiveCaptionText
+        btnSelectFolder.Location = New Point(12, 8)
+        btnSelectFolder.Name = "btnSelectFolder"
+        btnSelectFolder.Size = New Size(108, 32)
+        btnSelectFolder.TabIndex = 0
+        btnSelectFolder.Text = "Sellect folder"
+        btnSelectFolder.UseVisualStyleBackColor = True
+        ' 
+        ' btnStart
+        ' 
+        btnStart.ForeColor = SystemColors.ActiveCaptionText
+        btnStart.Location = New Point(880, 833)
+        btnStart.Name = "btnStart"
+        btnStart.Size = New Size(131, 42)
+        btnStart.TabIndex = 2
+        btnStart.Text = "Embed Lyrics"
+        btnStart.UseVisualStyleBackColor = True
+        ' 
+        ' lstLog
+        ' 
+        lstLog.BackColor = SystemColors.ControlDarkDark
+        lstLog.ForeColor = SystemColors.Window
+        lstLog.FormattingEnabled = True
+        lstLog.ItemHeight = 15
+        lstLog.Location = New Point(12, 43)
+        lstLog.Name = "lstLog"
+        lstLog.Size = New Size(657, 379)
+        lstLog.TabIndex = 3
+        ' 
+        ' ProgressBar
+        ' 
+        ProgressBar.Location = New Point(14, 443)
+        ProgressBar.Name = "ProgressBar"
+        ProgressBar.Size = New Size(655, 23)
+        ProgressBar.Style = ProgressBarStyle.Continuous
+        ProgressBar.TabIndex = 4
+        ' 
+        ' lblProgress
+        ' 
+        lblProgress.AutoSize = True
+        lblProgress.Location = New Point(821, 313)
+        lblProgress.Name = "lblProgress"
+        lblProgress.Size = New Size(30, 15)
+        lblProgress.TabIndex = 5
+        lblProgress.Text = "0 / 0"
+        ' 
+        ' txtFolder
+        ' 
+        txtFolder.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        txtFolder.Location = New Point(126, 14)
+        txtFolder.Name = "txtFolder"
+        txtFolder.Size = New Size(543, 23)
+        txtFolder.TabIndex = 6
+        txtFolder.Text = "Please Sellect Folder To Scan"
+        ' 
+        ' lblFailed
+        ' 
+        lblFailed.AutoSize = True
+        lblFailed.Location = New Point(821, 352)
+        lblFailed.Name = "lblFailed"
+        lblFailed.Size = New Size(13, 15)
+        lblFailed.TabIndex = 8
+        lblFailed.Text = "0"
+        ' 
+        ' lvFileResults
+        ' 
+        lvFileResults.BackColor = SystemColors.ControlDarkDark
+        lvFileResults.Columns.AddRange(New ColumnHeader() {ColumnHeader1, ColumnHeader2})
+        lvFileResults.ForeColor = SystemColors.Window
+        lvFileResults.FullRowSelect = True
+        lvFileResults.GridLines = True
+        lvFileResults.Location = New Point(14, 496)
+        lvFileResults.Name = "lvFileResults"
+        lvFileResults.Size = New Size(655, 379)
+        lvFileResults.TabIndex = 9
+        lvFileResults.UseCompatibleStateImageBehavior = False
+        lvFileResults.View = View.Details
+        ' 
+        ' ColumnHeader1
+        ' 
+        ColumnHeader1.Text = "File Name"
+        ColumnHeader1.Width = 250
+        ' 
+        ' ColumnHeader2
+        ' 
+        ColumnHeader2.Text = "Status"
+        ColumnHeader2.Width = 400
+        ' 
+        ' numMaxConcurrent
+        ' 
+        numMaxConcurrent.Location = New Point(900, 10)
+        numMaxConcurrent.Maximum = New Decimal(New Integer() {500, 0, 0, 0})
+        numMaxConcurrent.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        numMaxConcurrent.Name = "numMaxConcurrent"
+        numMaxConcurrent.Size = New Size(50, 23)
+        numMaxConcurrent.TabIndex = 10
+        numMaxConcurrent.Value = New Decimal(New Integer() {5, 0, 0, 0})
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(719, 17)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(175, 15)
+        Label1.TabIndex = 11
+        Label1.Text = "LRCLIB Max Concurrent Queries"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = SystemColors.ControlDarkDark
+        TextBox1.ForeColor = SystemColors.Window
+        TextBox1.Location = New Point(675, 43)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(336, 235)
+        TextBox1.TabIndex = 12
+        TextBox1.Text = resources.GetString("TextBox1.Text")
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(682, 313)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(86, 15)
+        Label2.TabIndex = 13
+        Label2.Text = "Files Processed"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(682, 352)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(64, 15)
+        Label3.TabIndex = 14
+        Label3.Text = "Files Failed"
+        ' 
+        ' chkFallbackUnsynced
+        ' 
+        chkFallbackUnsynced.AutoSize = True
+        chkFallbackUnsynced.Location = New Point(682, 392)
+        chkFallbackUnsynced.Name = "chkFallbackUnsynced"
+        chkFallbackUnsynced.Size = New Size(235, 19)
+        chkFallbackUnsynced.TabIndex = 15
+        chkFallbackUnsynced.Text = "Use unsynced lyrics if synced not found"
+        chkFallbackUnsynced.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        AutoSize = True
+        BackColor = SystemColors.ControlDarkDark
+        ClientSize = New Size(1021, 881)
+        Controls.Add(chkFallbackUnsynced)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Controls.Add(numMaxConcurrent)
+        Controls.Add(lvFileResults)
+        Controls.Add(lblFailed)
+        Controls.Add(txtFolder)
+        Controls.Add(lblProgress)
+        Controls.Add(ProgressBar)
+        Controls.Add(lstLog)
+        Controls.Add(btnStart)
+        Controls.Add(btnSelectFolder)
+        ForeColor = SystemColors.ButtonHighlight
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        Name = "Form1"
+        Text = "LYRSYNC V1.0"
+        CType(numMaxConcurrent, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnSelectFolder As Button
+    Friend WithEvents btnStart As Button
+    Friend WithEvents lstLog As ListBox
+    Friend WithEvents ProgressBar As ProgressBar
+    Friend WithEvents lblProgress As Label
+    Friend WithEvents txtFolder As TextBox
+    Friend WithEvents lblFailed As Label
+    Friend WithEvents lvFileResults As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents numMaxConcurrent As NumericUpDown
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents chkFallbackUnsynced As CheckBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,457 @@
+﻿Imports System.Collections
+Imports System.IO
+Imports System.Net
+Imports System.Text.RegularExpressions
+Imports System.Threading
+Imports System.Threading.Tasks
+Imports Newtonsoft.Json.Linq
+
+Imports TagLib
+
+Public Class Form1
+    ' Holds the selected folder path
+    Private selectedFolder As String = ""
+    ' Handles task cancellation
+    Private cancelTokenSource As CancellationTokenSource
+    Private cancelToken As CancellationToken
+    ' Indicates whether the tagging process is running
+    Private taggingRunning As Boolean = False
+    ' Total number of files to process
+    Private totalFiles As Integer = 0
+    ' Count of completed file processing
+    Private completedFiles As Integer = 0
+    ' Count of failed file processing
+    Private failedFiles As Integer = 0
+    ' Lock object for thread safety
+    Private lockObj As New Object()
+    ' Controls concurrency
+    Private semaphore As SemaphoreSlim
+    ' Tracks sorting column for ListView
+    Private sortColumn As Integer = -1
+    Private sortOrder As SortOrder = SortOrder.Ascending
+
+    ' Opens a folder dialog to select a folder containing FLAC files
+    Private Sub btnSelectFolder_Click(sender As Object, e As EventArgs) Handles btnSelectFolder.Click
+        Using fbd As New FolderBrowserDialog()
+            If fbd.ShowDialog() = DialogResult.OK Then
+                selectedFolder = fbd.SelectedPath
+                txtFolder.Text = selectedFolder
+
+                Dim audioFiles = Directory.GetFiles(selectedFolder, "*.*", SearchOption.AllDirectories)
+                Dim validFiles = audioFiles.Where(Function(f) f.EndsWith(".flac", StringComparison.OrdinalIgnoreCase) OrElse
+                                                f.EndsWith(".mp3", StringComparison.OrdinalIgnoreCase)).ToArray()
+
+                lvFileResults.Items.Clear()
+                For Each file In validFiles
+                    Dim item As New ListViewItem(Path.GetFileName(file))
+                    item.SubItems.Add("Not processed")
+                    item.ForeColor = Color.White
+                    lvFileResults.Items.Add(item)
+                Next
+
+                lblProgress.Text = $"0 processed / {validFiles.Length} files to scan"
+                failedFiles = 0
+                UpdateFailedLabel()
+            End If
+        End Using
+    End Sub
+
+
+    ' Starts or cancels the tagging process
+    Private Async Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        If taggingRunning Then
+            cancelTokenSource?.Cancel()
+            UpdateLog("Cancellation requested...")
+            btnStart.Enabled = False
+            Return
+        End If
+
+        If String.IsNullOrEmpty(selectedFolder) OrElse Not Directory.Exists(selectedFolder) Then
+            MessageBox.Show("Please select a valid folder.")
+            Return
+        End If
+
+        cancelTokenSource = New CancellationTokenSource()
+        cancelToken = cancelTokenSource.Token
+        taggingRunning = True
+        btnStart.Text = "Cancel"
+        btnStart.Enabled = True
+
+        ProgressBar.Value = 0
+        lstLog.Items.Clear()
+        lvFileResults.Items.Clear()
+        lstLog.Items.Add("Starting tagging...")
+
+        completedFiles = 0
+        failedFiles = 0
+        UpdateFailedLabel()
+
+        ' Set maximum concurrency based on user input
+        semaphore = New SemaphoreSlim(CInt(numMaxConcurrent.Value))
+
+        ' Start processing all files asynchronously
+        Await ProcessAllFiles(selectedFolder, cancelToken)
+
+        taggingRunning = False
+        Invoke(Sub()
+                   btnStart.Text = "Start Tagging"
+                   btnStart.Enabled = True
+               End Sub)
+
+        lstLog.Items.Add("Finished.")
+        lstLog.TopIndex = lstLog.Items.Count - 1
+    End Sub
+
+    ' Runs tagging on all .flac files concurrently using a semaphore
+    Async Function ProcessAllFiles(folderPath As String, token As CancellationToken) As Task
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
+        Dim flacFiles = Directory.GetFiles(folderPath, "*.flac", SearchOption.AllDirectories)
+        totalFiles = flacFiles.Length
+
+        Invoke(Sub()
+                   lblProgress.Text = $"0 processed / {totalFiles} files to scan"
+                   ProgressBar.Maximum = totalFiles
+                   ProgressBar.Value = 0
+               End Sub)
+
+        UpdateProgress()
+
+        If totalFiles = 0 Then
+            UpdateLog("No FLAC files found.")
+            Return
+        End If
+
+        Dim tasks As New List(Of Task)
+        For Each filePath In flacFiles
+            If token.IsCancellationRequested Then Exit For
+            Dim localPath = filePath
+            tasks.Add(Task.Run(Async Function()
+                                   Await semaphore.WaitAsync()
+                                   Try
+                                       ProcessFile(localPath, token)
+                                   Finally
+                                       semaphore.Release()
+                                   End Try
+                               End Function))
+        Next
+        Await Task.WhenAll(tasks)
+    End Function
+
+    ' Processes a single FLAC file, queries the LRClib API, and writes lyrics to the tag
+    Sub ProcessFile(filePath As String, token As CancellationToken)
+        If token.IsCancellationRequested Then Return
+
+        Try
+            Dim tfile = TagLib.File.Create(filePath)
+            Dim rawArtist = tfile.Tag.FirstPerformer
+            Dim rawTitle = tfile.Tag.Title
+
+            If String.IsNullOrWhiteSpace(rawArtist) OrElse String.IsNullOrWhiteSpace(rawTitle) Then
+                Dim msg = "Missing artist or title tag"
+                LogFailure(Path.GetFileName(filePath), msg)
+                UpdateLog($"{Path.GetFileName(filePath)}: {msg}")
+                Return
+            End If
+
+            Dim artist = CleanTag(rawArtist)
+            Dim title = CleanTag(rawTitle)
+            Dim localDuration = CInt(tfile.Properties.Duration.TotalSeconds)
+            Dim url = $"https://lrclib.net/api/search?track_name={Uri.EscapeDataString(title)}&artist_name={Uri.EscapeDataString(artist)}"
+            UpdateLog($"Querying: {artist} - {title}")
+
+            Dim jsonText As String = ""
+            Using client As New WebClient()
+                jsonText = client.DownloadString(url)
+            End Using
+
+            Dim jsonArray = JArray.Parse(jsonText)
+            Dim bestMatch As JObject = Nothing
+            Dim highestScore As Integer = -1
+            Dim finalLyrics As String = ""
+            Dim bestLyricsType As String = ""
+
+            ' Try synced lyrics first
+            For Each result As JObject In jsonArray
+                Dim lyricsSync = result.Value(Of String)("syncedLyrics")
+                Dim resArtist = result.Value(Of String)("artist_name")?.ToLower()
+                Dim resTitle = result.Value(Of String)("track_name")?.ToLower()
+                Dim resultDuration = result.Value(Of Integer?)("duration").GetValueOrDefault(0)
+                If String.IsNullOrWhiteSpace(lyricsSync) OrElse resultDuration = 0 Then Continue For
+
+                Dim score As Integer = 0
+                If resArtist = artist.ToLower() Then
+                    score += 2
+                ElseIf resArtist?.Contains(artist.ToLower()) Then
+                    score += 1
+                End If
+                If resTitle = title.ToLower() Then
+                    score += 2
+                ElseIf resTitle?.Contains(title.ToLower()) Then
+                    score += 1
+                End If
+
+                Dim delta = Math.Abs(localDuration - resultDuration)
+                If delta < 2 Then
+                    score += 3
+                ElseIf delta < 5 Then
+                    score += 2
+                ElseIf delta < 10 Then
+                    score += 1
+                End If
+
+                If score > highestScore Then
+                    bestMatch = result
+                    highestScore = score
+                    finalLyrics = lyricsSync
+                    bestLyricsType = "synced"
+                End If
+            Next
+
+            ' Try unsynced if fallback is enabled
+            If bestMatch Is Nothing AndAlso chkFallbackUnsynced.Checked Then
+                highestScore = -1
+                For Each result As JObject In jsonArray
+                    Dim lyricsPlain = result.Value(Of String)("lyrics")
+                    Dim resArtist = result.Value(Of String)("artist_name")?.ToLower()
+                    Dim resTitle = result.Value(Of String)("track_name")?.ToLower()
+                    Dim resultDuration = result.Value(Of Integer?)("duration").GetValueOrDefault(0)
+                    If String.IsNullOrWhiteSpace(lyricsPlain) OrElse resultDuration = 0 Then Continue For
+
+                    UpdateLog($"Checked fallback result: {resArtist} - {resTitle} (lyrics length: {If(lyricsPlain Is Nothing, 0, lyricsPlain.Length)})")
+
+                    Dim score As Integer = 0
+                    If resArtist = artist.ToLower() Then
+                        score += 2
+                    ElseIf resArtist?.Contains(artist.ToLower()) Then
+                        score += 1
+                    End If
+                    If resTitle = title.ToLower() Then
+                        score += 2
+                    ElseIf resTitle?.Contains(title.ToLower()) Then
+                        score += 1
+                    End If
+
+                    Dim delta = Math.Abs(localDuration - resultDuration)
+                    If delta < 2 Then
+                        score += 3
+                    ElseIf delta < 5 Then
+                        score += 2
+                    ElseIf delta < 10 Then
+                        score += 1
+                    End If
+
+                    If score > highestScore Then
+                        bestMatch = result
+                        highestScore = score
+                        finalLyrics = lyricsPlain
+                        bestLyricsType = "unsynced (lrclib)"
+                    End If
+                Next
+            End If
+
+            ' Try Lyrics.ovh as final fallback
+            If bestMatch Is Nothing AndAlso chkFallbackUnsynced.Checked Then
+                Dim ovhLyrics = FetchUnsyncedLyricsFromLyricsOvh(artist, title)
+                If Not String.IsNullOrWhiteSpace(ovhLyrics) Then
+                    finalLyrics = ovhLyrics
+                    bestLyricsType = "unsynced (Lyrics.ovh)"
+                    UpdateLog($"Used fallback from Lyrics.ovh for: {artist} - {title}")
+                End If
+            End If
+
+            If String.IsNullOrWhiteSpace(finalLyrics) Then
+                Dim msg = "No suitable lyrics found"
+                LogFailure(Path.GetFileName(filePath), msg)
+                UpdateLog($"{Path.GetFileName(filePath)}: {msg}")
+                Return
+            End If
+
+            SyncLock lockObj
+                tfile.Tag.Lyrics = finalLyrics
+                tfile.Save()
+            End SyncLock
+
+            LogSuccess(Path.GetFileName(filePath), "Tagged successfully (" & bestLyricsType & ")")
+            UpdateLog($"Tagged: {artist} - {title}")
+
+        Catch ex As Exception
+            LogFailure(Path.GetFileName(filePath), $"Error: {ex.Message}")
+            UpdateLog($"{Path.GetFileName(filePath)}: Error - {ex.Message}")
+        Finally
+            Interlocked.Increment(completedFiles)
+            UpdateProgress()
+        End Try
+    End Sub
+
+    Function FetchUnsyncedLyricsFromLyricsOvh(artist As String, title As String) As String
+        Try
+            Dim url = $"https://api.lyrics.ovh/v1/{Uri.EscapeDataString(artist)}/{Uri.EscapeDataString(title)}"
+            Using client As New WebClient()
+                Dim jsonText = client.DownloadString(url)
+                Dim json = JObject.Parse(jsonText)
+                Dim lyrics = json.Value(Of String)("lyrics")
+                Return If(String.IsNullOrWhiteSpace(lyrics), Nothing, lyrics)
+            End Using
+        Catch ex As Exception
+            UpdateLog($"Lyrics.ovh error: {ex.Message}")
+        End Try
+        Return Nothing
+    End Function
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ' Normalizes and cleans the artist/title metadata to improve API matching
+    Function CleanTag(input As String) As String
+        If String.IsNullOrWhiteSpace(input) Then Return ""
+
+        ' Remove bracketed/parenthetical text
+        input = System.Text.RegularExpressions.Regex.Replace(input, "\[.*?\]|\(.*?\)", "")
+
+        ' Remove featured artist info
+        input = System.Text.RegularExpressions.Regex.Replace(input, "(feat\.?|ft\.?|featuring)\s+[^\-]+", "", RegexOptions.IgnoreCase)
+
+        ' Replace special/bad characters
+        Dim badChars As New Dictionary(Of String, String) From {
+        {Chr(34), ""},  ' " (double quote)
+        {"'", ""},
+        {"“", ""}, {"”", ""}, {"‘", ""}, {"’", ""},
+        {"/", " "}, {"\", " "}, {"|", " "}, {"?", ""}, {"*", ""},
+        {":", " "}, {"<", ""}, {">", ""}
+    }
+
+        For Each kvp In badChars
+            input = input.Replace(kvp.Key, kvp.Value)
+        Next
+
+        ' Remove control characters
+        input = New String(input.Where(Function(c) Not Char.IsControl(c)).ToArray())
+
+        ' Normalize whitespace
+        input = System.Text.RegularExpressions.Regex.Replace(input, "\s+", " ").Trim()
+
+        Return input
+    End Function
+
+
+    ' Updates progress bar and label in the UI
+    Sub UpdateProgress()
+        If InvokeRequired Then
+            Invoke(New Action(AddressOf UpdateProgress))
+        Else
+            ProgressBar.Value = Math.Min(completedFiles, totalFiles)
+            lblProgress.Text = $"{completedFiles} processed / {totalFiles} files to scan"
+        End If
+    End Sub
+
+    ' Appends a message to the log list box
+    Sub UpdateLog(msg As String)
+        If lstLog.InvokeRequired Then
+            lstLog.Invoke(Sub() UpdateLog(msg))
+        Else
+            lstLog.Items.Add(msg)
+            lstLog.TopIndex = lstLog.Items.Count - 1
+        End If
+    End Sub
+
+    ' Logs a failed tagging attempt and displays it in the UI
+    Sub LogFailure(fileName As String, reason As String)
+        Interlocked.Increment(failedFiles)
+        UpdateFailedLabel()
+        If lvFileResults.InvokeRequired Then
+            lvFileResults.Invoke(Sub() LogFailure(fileName, reason))
+        Else
+            Dim item As New ListViewItem(fileName)
+            item.SubItems.Add(reason)
+            item.ForeColor = If(reason.ToLower().Contains("error"), Color.Red, Color.DarkGoldenrod)
+            lvFileResults.Items.Add(item)
+            lvFileResults.EnsureVisible(lvFileResults.Items.Count - 1) ' Auto-scroll here
+        End If
+    End Sub
+
+    Sub LogSuccess(fileName As String, message As String)
+        If lvFileResults.InvokeRequired Then
+            lvFileResults.Invoke(Sub() LogSuccess(fileName, message))
+        Else
+            Dim item As New ListViewItem(fileName)
+            item.SubItems.Add(message)
+            item.ForeColor = Color.LimeGreen
+            lvFileResults.Items.Add(item)
+            lvFileResults.EnsureVisible(lvFileResults.Items.Count - 1) ' Auto-scroll here
+        End If
+    End Sub
+
+
+
+
+    ' Updates the failed file count label
+    Sub UpdateFailedLabel()
+        If lblFailed.InvokeRequired Then
+            lblFailed.Invoke(Sub() UpdateFailedLabel())
+        Else
+            lblFailed.Text = $"Failed: {failedFiles}"
+        End If
+    End Sub
+
+    ' Enables sorting when a ListView column header is clicked
+    Private Sub lvFailedFiles_ColumnClick(sender As Object, e As ColumnClickEventArgs) Handles lvFileResults.ColumnClick
+        If e.Column = sortColumn Then
+            sortOrder = If(sortOrder = SortOrder.Ascending, SortOrder.Descending, SortOrder.Ascending)
+        Else
+            sortColumn = e.Column
+            sortOrder = SortOrder.Ascending
+        End If
+        lvFileResults.ListViewItemSorter = New ListViewItemComparer(sortColumn, sortOrder)
+        lvFileResults.Sort()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lstLog.DrawMode = DrawMode.OwnerDrawFixed
+        AddHandler lstLog.DrawItem, AddressOf lstLog_DrawItem
+    End Sub
+    Private Sub lstLog_DrawItem(sender As Object, e As DrawItemEventArgs)
+        If e.Index < 0 Then Return
+        Dim lb As ListBox = CType(sender, ListBox)
+        Dim text As String = lb.Items(e.Index).ToString()
+
+        e.DrawBackground()
+
+        Dim textColor As Brush = Brushes.White
+        If text = "Finished." Then
+            textColor = Brushes.LimeGreen
+        End If
+
+        e.Graphics.DrawString(text, e.Font, textColor, e.Bounds.Location)
+
+        e.DrawFocusRectangle()
+    End Sub
+End Class
+
+
+' Custom ListView sorter class
+Public Class ListViewItemComparer
+    Implements IComparer
+    Private col As Integer
+    Private order As SortOrder
+
+    Public Sub New(column As Integer, sortOrder As SortOrder)
+        col = column
+        order = sortOrder
+    End Sub
+
+    Public Function Compare(x As Object, y As Object) As Integer Implements IComparer.Compare
+        Dim itemX As ListViewItem = CType(x, ListViewItem)
+        Dim itemY As ListViewItem = CType(y, ListViewItem)
+        Dim result As Integer = String.Compare(itemX.SubItems(col).Text, itemY.SubItems(col).Text)
+        Return If(order = SortOrder.Descending, -result, result)
+    End Function
+End Class
